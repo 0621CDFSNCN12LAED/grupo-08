08baef263cebcc6bb094cc073b5f2678553d375c
martinzazpe <martinzazpe1@gmail.com>
2021-08-18 00:20:40 +0200

    primer commit con register casi terminado

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.listen(3000, ()=>{
+console.log('servidor funcionando')
+})
+
+app.use(express.static(path.join(__dirname,'public')));
+
+app.get('/',(req,res) =>{
+res.sendFile(path.join(__dirname,'/views/login.html'))
+});
+
+app.get('/register',(req,res) =>{
+    res.sendFile(path.join(__dirname,'/views/register.html'))
+    });
