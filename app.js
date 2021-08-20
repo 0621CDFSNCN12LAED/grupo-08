d9a7680844ac8f984144be6cec0d6ba6755e508b
martinzazpe <martinzazpe1@gmail.com>
2021-08-20 23:36:16 +0200

    login y register, con algun bug

--- app.js
+++ app.js
@@ -2,16 +2,20 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-app.listen(3000, ()=>{
-console.log('servidor funcionando')
+app.listen(3000, () => {
+    console.log('servidor funcionando')
 })
 
-app.use(express.static(path.join(__dirname,'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/',(req,res) =>{
-res.sendFile(path.join(__dirname,'/views/login.html'))
+app.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/login.html'))
 });
 
-app.get('/register',(req,res) =>{
-    res.sendFile(path.join(__dirname,'/views/register.html'))
-    });
+app.get('/register', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/register.html'))
+});
+
+app.get('/terminos', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/terminos.html'))
+});
