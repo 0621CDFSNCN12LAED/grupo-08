9dff1e03c221503bfaed5cfabcc5b5200971b1d9
Florencia-Zapata <florzapata04@gmail.com>
2021-08-21 07:28:06 -0300

    Carrito de Compras en html y css

--- app.js
+++ app.js
@@ -1,30 +1,29 @@
+const path = require("path");
+const express = require("express");
+const app = express();
 
-const path = require('path')
-const express = require('express')
-const app = express()
-
-app.listen(3000, ()=>{
-    console.log("Todo en orden")
-})
+app.listen(3000, () => {
+    console.log("Todo en orden");
+});
 
 app.use(express.static(path.join(__dirname, "./public")));
 
-
-app.get('/', (req,res) =>{
-    res.sendFile(path.resolve(__dirname, "./views/index.html"))
-})
-
-app.get('/register', (req, res) => {
-    res.sendFile(path.join(__dirname, '/views/register.html'))
+app.get("/", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "./views/index.html"));
 });
 
-
-app.get('/login', (req, res) => {
-    res.sendFile(path.join(__dirname, '/views/login.html'))
+app.get("/register", (req, res) => {
+    res.sendFile(path.join(__dirname, "/views/register.html"));
 });
 
-
-app.get('/terminos', (req, res) => {
-    res.sendFile(path.join(__dirname, '/views/terminos.html'))
+app.get("/login", (req, res) => {
+    res.sendFile(path.join(__dirname, "/views/login.html"));
 });
 
+app.get("/terminos", (req, res) => {
+    res.sendFile(path.join(__dirname, "/views/terminos.html"));
+});
+
+app.get("/productCart", (req, res) => {
+    res.sendFile(path.join(__dirname, "/views/productCart.html"));
+});
