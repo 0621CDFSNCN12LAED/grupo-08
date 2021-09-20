d6c1c454e26dc38e27835781cc97e6c8c213f75d
Franco Ezequiel <eze.cavs@hotmail.com>
2021-09-20 20:03:45 -0300

    DELETE implementado y funcionando, sigo trabajando para implementar el PUT

--- controllers/product-controller.js
+++ controllers/product-controller.js
@@ -18,28 +18,46 @@ const controllerProducts = {
         });
         res.render("indexProdDetail", { productDetail });
     },
-
+    //Editar producto
     modificarProducto: (req, res) => {
-        res.render("modificarProducto");
+        let productoAEditar = productos.find((producto) => {
+            return producto.id == req.params.id;
+        });
+        res.render("modificarProducto", { productoAEditar: productoAEditar });
     },
-    
-nuevoProducto: (req, res) => {
-    res.render("crearProducto");
-},
 
+    nuevoProducto: (req, res) => {
+        res.render("crearProducto");
+    },
 
-// Crea nuevos produtos. Aún no solucione que cargue imagenes.
+    // Crea nuevos produtos. Aún no solucione que cargue imagenes.
     crearNuevoProducto: (req, res) => {
         const productId = productos.length;
         const producto = {
-        id : productId + 1,
-        ...req.body,
+            id: productId + 1,
+            ...req.body,
         };
         productos.push(producto);
-		fs.writeFileSync(productsFilePath, JSON.stringify(productos));
-		res.redirect("/productos");
+        fs.writeFileSync(productsFilePath, JSON.stringify(productos));
+        res.redirect("/productos");
         console.log(req.body);
-    }
+    },
+
+    //Eliminar productos
+    delete: (req, res) => {
+        //Filtrar productos
+        const filterProductsForDeleteOne = productos.findIndex((producto) => {
+            return producto.id == req.params.id;
+        });
+        productos.splice(filterProductsForDeleteOne, 1);
+
+        //Guardar filtro en el archivo JSON
+        const jsonString = JSON.stringify(productos, null, 4);
+        fs.writeFileSync(productsFilePath, jsonString);
+
+        //Redirect
+        res.redirect("/");
+    },
 };
 
 module.exports = controllerProducts;
--- routes/product-routers.js
+++ routes/product-routers.js
@@ -2,27 +2,24 @@ const express = require("express");
 const router = express.Router();
 const controlladorProductos = require("../controllers/product-controller");
 
-// Ver todos los productos 
+// Ver todos los productos
 router.get("/productos", controlladorProductos.productos);
-
 
 // carrito del producto
 router.get("/productCart", controlladorProductos.productCart);
 
-
 //detalle del producto
 router.get("/productDetail/:id", controlladorProductos.productDetail);
 
-
 // crear un producto
 router.get("/crearProducto", controlladorProductos.nuevoProducto);
-router.post("/views/productos", controlladorProductos.crearNuevoProducto)
-
+router.post("/views/productos", controlladorProductos.crearNuevoProducto);
 
 // modificar un producto
-router.get("/modificarProducto", controlladorProductos.modificarProducto);
-
+router.get("/modificarProducto/:id", controlladorProductos.modificarProducto);
+router.put("/:id", controlladorProductos.modificarProducto);
 
 //borrar un producto
+router.delete("/:id", controlladorProductos.delete);
 
 module.exports = router;
