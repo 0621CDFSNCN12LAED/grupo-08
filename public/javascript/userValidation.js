25ad9fab054dc507b10ca6581b7862fafd5ccb43
Franco Ezequiel <eze.cavs@hotmail.com>
2021-11-24 19:03:24 -0300

    Avance validaciones, solo falta validar el tipo de dato de la imagen que sube el usuario y retocar los estilos

---
+++ public/javascript/userValidation.js
@@ -0,0 +1,138 @@
+let form = document.querySelector("#formRegister");
+let inputName = document.querySelector("#name");
+let apellido = document.querySelector("#apellido");
+let email = document.querySelector("#email");
+let password = document.querySelector("#password");
+let imagen = document.querySelector("#imagen");
+let terminos = document.querySelector("#terminos");
+let btnSubmit = document.querySelector("#buttonSend");
+
+//Contenedor errores
+let erName = document.querySelector(".erName");
+let erLastName = document.querySelector(".erLastName");
+let erEmail = document.querySelector(".erEmail");
+let erPassword = document.querySelector(".erPassword");
+let erImageProfile = document.querySelector(".erImageProfile");
+let erTerminos = document.querySelector(".erTerminos");
+
+let errorContainer = document.querySelector("#errorContainer");
+
+let errores = {
+    name: "",
+    lastName: "",
+};
+
+password.addEventListener("keyup", () =>{
+      checkPassword(password.value);
+})
+
+
+
+function checkPassword(pass){
+    let strengthBar = document.querySelector(".progress-bar")
+    let strengthPassword = 0;
+    if(pass.match(/[a-z]+/)){
+        strengthPassword += 1
+    }
+    if(pass.match(/[A-Z]+/)){
+        strengthPassword += 1
+    }
+    if(pass.match(/[0-9]+/)){
+        strengthPassword += 1
+    }
+    if(pass.match(/[!@$%^&*()~<>?]+/)){
+        strengthPassword += 1
+    }
+    if(pass.length > 7){
+        strengthPassword += 1
+    }
+    
+    switch(strengthPassword){
+            case 1:
+                strengthBar.style = "width: 20%";
+                break
+            case 2:
+                strengthBar.style = "width: 40%";
+                break
+            case 3:
+                strengthBar.style = "width: 60%";
+                break
+            case 4:
+                strengthBar.style = "width: 80%";
+                break
+            case 5:
+                strengthBar.style = "width: 100%";
+                break
+    }
+}
+
+btnSubmit.addEventListener("click", e =>{
+    //La variable regex hace que corrobore que el mail que escribió el usuario sea válido
+    let regex = /^[^\s@]+@[^\s@]+$/;
+    let validationNumber = /[0-9]+/;
+    let validationLowercase = /[a-z]+/;
+    let validationUppercase = /[A-Z]+/;
+    let validationSpecialCaracter = /[~<>?!@$%^&*()]+/;
+    
+    e.preventDefault();
+    
+    //Validación Nombre
+    if(inputName.value.length < 1){
+        errores.name = "Es obligatorio que ingrese un Nombre";
+    }else if (inputName.value.length < 3){
+        errores.name = "El nombre debe contener almenos 3 caracteres";
+    }
+    //Validación Apellido
+    if(apellido.value.length < 1){
+        errores.lastName = "Es obligatorio que ingrese un Apellido";
+    }else if (apellido.value.length < 3){
+        errores.lastName = "El apellido debe contener almenos 3 caracteres";
+    }
+    //Validación email
+    if(email.value.length < 1){
+        errores.email = "Es obligatorio que ingrese un Email";
+    }else if(!regex.test(email.value)){
+        errores.email = "Debe ingresar un mail válido";
+    }
+    //Validación contraseña
+    if(password.value.length < 1){
+        errores.password = "Es obligatorio que ingrese una contraseña";
+    }else if (password.value.length < 8){
+        errores.password = "La contraseña debe poseer almenos 8 caracteres";
+    }else if(!validationUppercase.test(password.value)){
+        errores.password = "La contraseña debe contener almenos una mayuscula"
+    }
+    else if(!validationLowercase.test(password.value)){
+        errores.password = "La contraseña debe contener almenos una minúscula"
+    }
+    else if(!validationNumber.test(password.value)){
+        errores.password = "La contraseña debe contener almenos un número"
+    }
+    else if(!validationSpecialCaracter.test(password.value)){
+        errores.password = "La contraseña debe contener almenos un caracter raro"
+    }
+    //Validación Imagen
+    if(imagen.value != undefined || imagen.value != null){
+        errores.imagen = "Es obligatorio que adjunte una imagen";
+    }else if (imagen.value != "jpg" || imagen.value != "jpeg" || imagen.value != "png" || imagen.value != "Gif"){
+        errores.imagen = "El formato del archivo no es válido, solo se aceptan JPG, JPEG, PNG o GIF";
+    }
+    //Validación Terminos y condiciones
+    if(terminos.value.length < 1){
+        errores.terminos = "Es necesario que acepte los términos y condiciones";
+    }
+
+
+
+
+    if(Object.keys(errores).length >= 1){
+        erName.innerText = (errores.name) ? errores.name : "";
+        erLastName.innerText = (errores.lastName) ? errores.lastName : "";
+        erEmail.innerText = (errores.email) ? errores.email : "";
+        erPassword.innerText = (errores.password) ? errores.password : "";
+        //erImageProfile.innerText = (errores.imagen) ? errores.imagen : "";
+        //erTerminos.innerText = (errores.terminos) ? errores.terminos : "";
+    } else{
+        form.submit();
+    }
+})
