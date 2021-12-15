18cc9f4cc7540d7915ee27507f2da3721061bc69
fedehonrado <fede.honrado@gmail.com>
2021-12-15 19:12:23 -0300

    cambio estetico login

--- dashboard/src/components/NavBar/NavBar.js
+++ dashboard/src/components/NavBar/NavBar.js
@@ -38,8 +38,8 @@ function NavBar() {
 						{/*<!-- Nav Item - User Information -->*/}
 						<li className="nav-item dropdown no-arrow">
 							<a className="nav-link dropdown-toggle" href="/" id="userDropdown">
-								<span className="mr-2 d-none d-lg-inline text-gray-600 small">Jordan Walke</span>
-								<img className="img-profile rounded-circle" src="" alt="Jordan Walke - Creador de React" width="60"/>
+								<span className="mr-2 d-none d-lg-inline text-gray-600 small">Registrarse/Log in</span>
+								<img className="img-profile rounded-circle" src="" alt="" width="60"/>
 							</a>
 						</li>
 
